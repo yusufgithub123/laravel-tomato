abb51bc0f2e8a18c5f0e32cb22cad4b23532bbd9
ihsanbisbox <ihsanbisbox@gmail.com>
2025-05-23 21:53:21 +0800

    menambahkan modifikasi dihalaman beranda

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -4,11 +4,24 @@
             <div class="slide-content">
                 <div class="slide-text">
                     <h2>LEAFGUARD TOMATO</h2>
-                    <p>LEAFGUARD-TOMATO adalah sistem klasifikasi otomatis...</p>
+                    <p>LEAFGUARD-TOMATO adalah sistem klasifikasi otomatis berbasis deep learning yang dirancang khusus untuk membantu petani tomat di Indonesia dalam mengidentifikasi dan mengatasi penyakit daun tomat secara cepat, akurat, dan mudah.</p>
                     <button class="swipe-btn">SWIPE</button>
                 </div>
                 <div class="slide-image">
                     <img src="{{ asset('assets/images/foto.png') }}" alt="Tomato">
+                </div>
+            </div>
+        </div>
+
+        <div class="slide">
+            <div class="slide-content">
+                <div class="slide-text">
+                    <h2>DETEKSI PENYAKIT AKURAT</h2>
+                    <p>Menggunakan teknologi deep learning terdepan untuk mendeteksi berbagai jenis penyakit pada daun tomat dengan tingkat akurasi tinggi dan hasil yang dapat diandalkan.</p>
+                    <button class="swipe-btn">SWIPE</button>
+                </div>
+                <div class="slide-image">
+                    <img src="{{ asset('assets/images/foto1.jpeg') }}" alt="Tomato Plant">
                 </div>
             </div>
         </div>
